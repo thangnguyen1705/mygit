8c4e71a8bd5ebdb505dee7cba24d956362b3a6a2
@Thangnguyen1995 <thanghong195@gmail.com>
2020-03-24 13:17:26 +0700

    first commit

---
+++ thangnv/PCI.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+text = /tmp/pci.txt
+date > $text
+echo "-----------------" >> $text
+uname -r >> $text
+echo "-----------------" >> $text
+cat /etc/passwd | grep /*sh$ | awk -F: '{print $1}' >> /tmp/pci.txt
+echo "awk -F: '($2 == "") {print $1}' /etc/shadow" >> /tmp/pci.txt
+awk -F: '($2 == "") {print $1}' /etc/shadow >> 
+
