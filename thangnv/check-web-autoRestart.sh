f7e87d72e15b43cdbbf954f847dd8f3a165316ee
thangnguyen1705 <thanghong195@gmail.com>
2020-06-16 10:47:57 +0700

    ok

--- thangnv/check-web-autoRestart.sh
+++ thangnv/check-web-autoRestart.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+cout=1
+while [ $cout -lt 10 ]
+do
+       status=`curl -s -o /dev/null -w "%{http_code}" http://47.244.169.77:8080/api/users/check`
+       echo `date` $status >> /tmp/crash_checks.log
 
-status=`curl -s -o /dev/null -w "%{http_code}" https://example.com`
-echo `date` $status >> crash_checks.log
+       if [ "$status" -ne "200" ]
+       then
+              # Take any appropriate recovery action here.
+              echo "webserver seems down, $cout th"
+              let cout++
+       else
+              echo "web ok"
+              break
+       fi
 
-if [ "$status" -ne "200" ]
-then
-       # Take any appropriate recovery action here.
-	echo "webserver seems down, initiating reboot." >> check.log
-fi
+       if [ $cout > 3 ]
+       then
+              echo " chay lenh"
+       fi
+done
