aa4cbcf46b3cf4e36951c6ef833d5bfc59176953
thangnguyen1705 <thanghong195@gmail.com>
2020-06-17 08:34:15 +0700

    Update check-web-autoRestart.sh

--- thangnv/check-web-autoRestart.sh
+++ thangnv/check-web-autoRestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cout=1
-while [ $cout -lt 10 ]
+while [ $cout -lt 4 ]
 do
-       status=`curl -s -o /dev/null -w "%{http_code}" http://47.244.169.77:8080/api/users/check`
+       status=`curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/api/users/check`
        echo `date` $status >> /tmp/crash_checks.log
 
        if [ "$status" -ne "200" ]
@@ -11,13 +11,13 @@ do
               echo "webserver seems down, $cout th"
               let cout++
        else
-              echo "web ok"
+              echo "web ok" >> /tmp/crash_checks.log
               break
        fi
 
-       if [ $cout > 3 ]
+       if [ $cout == 4 ]
        then
-              echo " chay lenh"
+              echo " chay lenh" >> /tmp/crash_checks.log
               pkill -9 -f tomcat
               sh /opt/tomcat/bin/startup.sh
        fi
