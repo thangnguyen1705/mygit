0fa3f04e57d7e6cdf5c94c2497ce2630f91922ab
thangnguyen1705 <thanghong195@gmail.com>
2020-06-20 10:59:00 +0700

    Update check-web-autoRestart.sh

--- thangnv/check-web-autoRestart.sh
+++ thangnv/check-web-autoRestart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cout=1
+port=8104
 while [ $cout -lt 4 ]
 do
-       status=`curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/api/users/check`
+       status=`curl -m 5 -s -o /dev/null -w "%{http_code}" http://127.0.0.1:$port/api/users/check`
        echo `date` $status >> /tmp/crash_checks.log
 
        if [ "$status" -ne "200" ]
@@ -17,8 +18,9 @@ do
 
        if [ $cout == 4 ]
        then
-              echo " chay lenh" >> /tmp/crash_checks.log
-              pkill -9 -f tomcat
-              sh /opt/tomcat/bin/startup.sh
+              id=`netstat -lnput |  grep $port | awk '{print $7}'| sed "s/\/java//g"`
+              echo "restart tomcat pidid:" $id >> /tmp/crash_checks.log
+              kill -9 $id
+              sh /opt/tomcat4/bin/startup.sh
        fi
 done
