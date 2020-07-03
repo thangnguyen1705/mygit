023477a492e236cf2670e00e1ef9de767541ad6d
thangnguyen1705 <thanghong195@gmail.com>
2020-07-03 11:39:59 +0700

    Update Create-user-ansible.sh

---
+++ thangnv/Create-user-ansible.sh
@@ -0,0 +1,13 @@
+#bash
+echo "Nhung thong tin khong co hay an enter de bo qua"
+echo " nhap du an "
+read projects
+echo "nhap user:"
+read user
+echo "nhap mat khau:"
+read pass
+echo "nhap yubikey:"
+read yubikey
+echo "nhap public-key:"
+read public
+
