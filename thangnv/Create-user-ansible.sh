c96f3f76cd70bf50283584fed8d72ae882cb60ea
thangnguyen1705 <thanghong195@gmail.com>
2020-07-06 15:58:46 +0700

    Update Create-user-ansible.sh

--- thangnv/Create-user-ansible.sh
+++ thangnv/Create-user-ansible.sh
@@ -1,13 +1,62 @@
-#bash
-echo "Nhung thong tin khong co hay an enter de bo qua"
-echo " nhap du an "
-read projects
-echo "nhap user:"
-read user
-echo "nhap mat khau:"
-read pass
-echo "nhap yubikey:"
-read yubikey
-echo "nhap public-key:"
-read public
+#yubikey="vvjdfjdehjbl"
+#user="hoandx"
+#pass="h5kct26Sx"
 
+#useradd $user
+#echo -e $pass'\n'$pass | passwd $user
+#echo $user:$yubikey >> /etc/yubipasswd #key offline
+#echo $user 'ALL=(ALL)        ALL' >> /etc/sudoers
+
+checkuser=`cat /etc/passwd | grep $user`
+if [[ -z "$checkuser" ]] ; then
+    useradd $user
+fi
+
+if [[ ! -z "$pass" ]] ; then
+    echo -e $pass'\n'$pass | passwd $user
+    echo "pass"
+fi
+
+
+if [[ ! -z "$yubikey" ]] ; then
+    yubikey_old=`cat /etc/yubipasswd | grep $user | awk -F':' '{print $2}'| sed '$!d'`
+    if [[ "$yubikey" != "$yubikey_old" ]] ; then
+        sed -i 's/'$user'/\#'$user'/g'  /etc/yubipasswd
+        echo $user:$yubikey >> /etc/yubipasswd #key offline
+        echo "yubikey"
+    fi
+fi
+
+
+if [[ "$sudo" -eq "yes" ]] ; then
+    checksudo=`cat /etc/sudoers | grep $user`
+    checksudo1=`grep -r "#"$user /etc/sudoers`
+    echo "check sudo" $user
+    if [[ -z "$checksudo" ]] ; then
+        echo $user 'ALL=(ALL)        ALL' >> /etc/sudoers
+        echo "sudo "
+    fi
+    if [[ -z "$checksudo1" ]] ; then
+        sed -i 's/'$user'/\#'$user'/g' /etc/sudoers
+        echo "remove sudo "
+    fi
+fi
+
+if [[ "$sudo" -eq "no" ]] ; then
+        checksudo1=`grep -r "#"$user /etc/sudoers`
+        if [[ -z "$checksudo1" ]] ; then
+        sed -i 's/'$user'/\#'$user'/g' /etc/sudoers
+        echo "sudo remove"
+        if
+fi
+
+
+if [[ ! -z "$publickey" ]] ; then
+    cd /home/$user
+    mkdir ./.ssh
+    chmod 700 ./.ssh
+    touch ./.ssh/authorized_keys
+    chmod 600 ./.ssh/authorized_keys
+    echo $publickey >> /home/$user/.ssh/authorized_keys
+    echo "publickey"
+fi
