f9e971028a2f21c70f306fb9026e2dad56477b37
thangnguyen1705 <thanghong195@gmail.com>
2020-07-09 11:35:15 +0700

    update

---
+++ window/backups3.bat
@@ -0,0 +1,30 @@
+@Echo off
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+set "nam=%YYYY%"
+set "thang=%MM%" 
+set "ngay=%DD%"
+
+set userprofile=C:\Users\thangnv
+aws configure set AWS_ACCESS_KEY_ID AKIAY3LYZSBNNBF34XN6
+aws configure set AWS_SECRET_ACCESS_KEY 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
+aws configure set default.region ap-southeast-1
+
+Set "Pattern=ReportServerTempDB_backup_%nam%_%thang%_%ngay%"
+For %%A in ("G:\backup\ReportServerTempDB\*") Do Echo:%%~nA|findstr "%Pattern%">NUL 2>&1 && (
+	aws s3 cp %%~fA s3://pushsale-backup/ReportServerTempDB/
+	echo Ok > G:\backup\Crontab-backup\log.txt
+) || (
+    Echo No    Pattern in %%~fA
+)
+
+forfiles -p "G:\backup\ReportServerTempDB" -s -m *.bak -d -7 -c "cmd /c del @path"
+
+pause
+
