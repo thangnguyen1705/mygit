f89418e64dec292bb7cae215b57f3baf4f055f5c
thangnv <thangnv@peacesoft.net>
2020-12-24 15:39:27 +0700

    update

--- thangnv/kube_upfile.sh
+++ thangnv/kube_upfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's/nscore:nscore2020@128.199.70.237/mongo:4yrc6iG}74WaOGgh@mongodb.mongodb/g' *
 
-list=`find . -name "*.yaml" | sed  's/\.\///g'`
+list=`cat tmp.txt`
 for file in $list;do
     kubectl apply -f $file
 done
